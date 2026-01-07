fd6e464b64e4a95e9b612ec35ac670c1ccef17b7
Kim Nahyeon <nhyeonii@naver.com>
2026-01-07 12:36:01 +0900

    [FEAT/#5] CherrishCalendar 구현

---
+++ app/src/main/java/com/cherrish/android/presentation/calendar/component/CherrishCalendar.kt
@@ -0,0 +1,121 @@
+package com.cherrish.android.presentation.calendar.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.cherrish.android.core.designsystem.theme.CherrishTheme
+import com.cherrish.android.presentation.calendar.model.CalendarDay
+import com.cherrish.android.presentation.calendar.model.DownTimeStatus
+import com.cherrish.android.presentation.calendar.util.nextMonth
+import com.cherrish.android.presentation.calendar.util.previousMonth
+import java.time.LocalDate
+import java.time.YearMonth
+
+@Composable
+fun CherrishCalendar(
+    yearMonth: YearMonth,
+    selectedDate: LocalDate?,
+    onDateClick: (LocalDate) -> Unit,
+    onMonthChange: (YearMonth) -> Unit,
+    procedureCountByDate: Map<LocalDate, Int>,
+    downtimeByDate: Map<LocalDate, DownTimeStatus>,
+    modifier: Modifier = Modifier
+) {
+    val isDowntimeMode = downtimeByDate.isNotEmpty()
+
+    Column(modifier = modifier) {
+        CalendarHeader(
+            onLeftArrowClick = { onMonthChange(yearMonth.previousMonth) },
+            onRightArrowClick = { onMonthChange(yearMonth.nextMonth) },
+            yearMonth = yearMonth
+        )
+
+        BasicCalendar(
+            yearMonth = yearMonth,
+            procedureCountByDate = procedureCountByDate,
+            downtimeByDate = downtimeByDate,
+            dayContent = { day ->
+                DayItem(
+                    day = day,
+                    onClick = {
+                        if (day is CalendarDay.Date) {
+                            onDateClick(day.date)
+                        }
+                    },
+                    isSelected = day is CalendarDay.Date && day.date == selectedDate,
+                    showDowntime = isDowntimeMode
+                )
+            }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CherrishCalendarPreview() {
+    CherrishTheme {
+        var yearMonth by remember { mutableStateOf(YearMonth.now()) }
+        var selectedDate by remember { mutableStateOf<LocalDate?>(LocalDate.now()) }
+
+        val procedureCountByDate = remember {
+            mapOf(
+                LocalDate.now() to 2,
+                LocalDate.now().plusDays(1) to 1,
+                LocalDate.now().plusDays(5) to 3
+            )
+        }
+
+        CherrishCalendar(
+            yearMonth = yearMonth,
+            selectedDate = selectedDate,
+            procedureCountByDate = procedureCountByDate,
+            downtimeByDate = emptyMap(),
+            onDateClick = { selectedDate = it },
+            onMonthChange = { newMonth ->
+                yearMonth = newMonth
+            }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CherrishCalendarDowntimePreview() {
+    CherrishTheme {
+        var yearMonth by remember { mutableStateOf(YearMonth.now()) }
+        var selectedDate by remember { mutableStateOf<LocalDate?>(LocalDate.now()) }
+
+        val procedureCountByDate = remember {
+            mapOf(
+                LocalDate.now() to 2,
+                LocalDate.now().plusDays(1) to 1,
+                LocalDate.now().plusDays(5) to 3
+            )
+        }
+
+        val downtimeByDate = remember {
+            mapOf(
+                LocalDate.now() to DownTimeStatus.CAUTION,
+                LocalDate.now().plusDays(1) to DownTimeStatus.CAUTION,
+                LocalDate.now().plusDays(2) to DownTimeStatus.SENSITIVE,
+                LocalDate.now().plusDays(3) to DownTimeStatus.RECOVERY
+            )
+        }
+
+        CherrishCalendar(
+            yearMonth = yearMonth,
+            selectedDate = selectedDate,
+            procedureCountByDate = procedureCountByDate,
+            downtimeByDate = downtimeByDate,
+            onDateClick = { selectedDate = it },
+            onMonthChange = { newMonth ->
+                yearMonth = newMonth
+            }
+        )
+    }
+}
