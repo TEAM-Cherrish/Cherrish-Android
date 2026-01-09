890a85edefcaaa790ee7cd00d56be8546c361f73
sssuhha <ysh83@sookmyung.ac.kr>
2026-01-09 17:33:20 +0900

    [FEAT/#13] BackTopAppBar 구현

---
+++ app/src/main/java/com/cherrish/android/core/designsystem/component/BackTopAppBar.kt
@@ -0,0 +1,46 @@
+package com.cherrish.android.core.designsystem.component
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cherrish.android.R
+import com.cherrish.android.core.common.extension.noRippleClickable
+import com.cherrish.android.core.designsystem.theme.CherrishTheme
+
+@Composable
+fun BackTopAppBar(
+    modifier: Modifier = Modifier,
+    title: String?,
+    onBackClicked: () -> Unit
+) {
+    CherrishBasicTopAppBar(
+        modifier = modifier,
+        title = title,
+        navigationIcon = {
+            Icon(
+                modifier = Modifier
+                    .size(24.dp)
+                    .noRippleClickable(onClick = onBackClicked),
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_arrow_left_24),
+                contentDescription = null,
+                tint = CherrishTheme.colors.gray1000
+            )
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun BackTopAppBarPreview() {
+    CherrishTheme {
+        BackTopAppBar(
+            title = "시술 여부 선택",
+            onBackClicked = { }
+        )
+    }
+}
