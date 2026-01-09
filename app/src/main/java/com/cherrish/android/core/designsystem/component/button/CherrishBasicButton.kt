23c25176362c5134d2c8b278c669c5de69ac398e
Kim Nahyeon <nhyeonii@naver.com>
2026-01-09 12:21:05 +0900

    [FEAT/#6] CherrishBasicButton 구현

--- app/src/main/java/com/cherrish/android/core/designsystem/component/button/CherrishBasicButton.kt
+++ app/src/main/java/com/cherrish/android/core/designsystem/component/button/CherrishBasicButton.kt
@@ -1,143 +1,62 @@
 package com.cherrish.android.core.designsystem.component.button
 
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cherrish.android.core.designsystem.theme.CherrishTheme
-import com.cherrish.android.core.designsystem.type.CherrishButtonStyle
 
 @Composable
 fun CherrishBasicButton(
-    text: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    style: CherrishButtonStyle = CherrishButtonStyle.PRIMARY,
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+    contentPadding: PaddingValues = PaddingValues(10.dp),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    content: @Composable () -> Unit
 ) {
-    val cherrishColor = CherrishTheme.colors
-    val isPressed by interactionSource.collectIsPressedAsState()
-
-    val scale by animateFloatAsState(
-        targetValue = if (isPressed) 0.95f else 1f,
-        animationSpec = tween(durationMillis = 80),
-        label = "button-scale"
-    )
-
-    val backgroundColor = remember(enabled, style) {
-        when (style) {
-            CherrishButtonStyle.PRIMARY -> {
-                when {
-                    enabled -> cherrishColor.red700
-                    else -> cherrishColor.gray200
-                }
-            }
-            CherrishButtonStyle.SECONDARY -> {
-                cherrishColor.gray400
-            }
-        }
-    }
-
-    val textColor = remember(enabled, style) {
-        when (style) {
-            CherrishButtonStyle.PRIMARY -> {
-                when {
-                    enabled -> cherrishColor.gray0
-                    else -> cherrishColor.gray600
-                }
-            }
-            CherrishButtonStyle.SECONDARY -> {
-                cherrishColor.gray700
-            }
-        }
-    }
-
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .scale(scale = scale)
-            .clip(shape = RoundedCornerShape(12.dp))
-            .background(color = backgroundColor)
             .clickable(
                 enabled = enabled,
                 indication = null,
                 interactionSource = interactionSource,
                 onClick = onClick
             )
-            .padding(10.dp),
+            .padding(contentPadding),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(
-            text = text,
-            style = CherrishTheme.typography.title2SB16,
-            color = textColor
-        )
+        content()
     }
 }
 
 @Preview
 @Composable
-private fun CherrishBasicButtonsPreview() {
+private fun CherrishBasicButtonPreview() {
     CherrishTheme {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(10.dp)
-        ) {
-            CherrishBasicButton(
-                text = "다음",
-                onClick = {}
-            )
-            CherrishBasicButton(
-                text = "다음",
-                onClick = {},
-                enabled = false
-            )
-        }
-    }
-}
-
-@Preview
-@Composable
-private fun CherrishBasicButtonsInteractionPreview() {
-    CherrishTheme {
-        Row(
+        CherrishBasicButton(
+            onClick = {},
             modifier = Modifier
-                .fillMaxWidth()
-                .background(color = CherrishTheme.colors.gray0)
-                .padding(50.dp),
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-            verticalAlignment = Alignment.CenterVertically
+                .background(color = CherrishTheme.colors.red700, shape = RoundedCornerShape(8.dp))
         ) {
-            CherrishBasicButton(
-                text = "다운타임 없이 일정 추가",
-                onClick = {},
-                style = CherrishButtonStyle.SECONDARY,
-                modifier = Modifier.weight(184f)
-            )
-
-            CherrishBasicButton(
-                text = "확인",
-                onClick = {},
-                modifier = Modifier.weight(122f)
+            Text(
+                text = "다음",
+                style = CherrishTheme.typography.title2SB16,
+                color = CherrishTheme.colors.gray0
             )
         }
     }
