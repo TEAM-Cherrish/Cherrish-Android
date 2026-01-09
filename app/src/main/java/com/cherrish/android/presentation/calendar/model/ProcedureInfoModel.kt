1f4f50b1abbb26c0b82a5bba7372e18865640a9d
Kim Nahyeon <nhyeonii@naver.com>
2026-01-10 01:25:06 +0900

    [FEAT/#16] ProcedureInfoModel @Immutable 어노테이션 추가

--- app/src/main/java/com/cherrish/android/presentation/calendar/model/ProcedureInfoModel.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/model/ProcedureInfoModel.kt
@@ -1,5 +1,8 @@
 package com.cherrish.android.presentation.calendar.model
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class ProcedureInfoModel(
     val procedureId: Long,
     val procedureName: String,
