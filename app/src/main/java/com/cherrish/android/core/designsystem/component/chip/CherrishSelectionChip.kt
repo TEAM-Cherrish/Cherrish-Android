274971b5d7cb4ab4346241c270a92d662140603a
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-11 01:08:35 +0900

    [MOD/#8] ktLint 실행 및 코드리뷰 반영

--- app/src/main/java/com/cherrish/android/core/designsystem/component/chip/CherrishSelectionChip.kt
+++ app/src/main/java/com/cherrish/android/core/designsystem/component/chip/CherrishSelectionChip.kt
@@ -1,7 +1,5 @@
 package com.cherrish.android.core.designsystem.component.chip
 
-import android.media.Image
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
@@ -9,8 +7,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,13 +18,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cherrish.android.R
 import com.cherrish.android.core.common.extension.noRippleClickable
 import com.cherrish.android.core.designsystem.theme.CherrishTheme
+import com.cherrish.android.core.designsystem.theme.gray500
+import com.cherrish.android.core.designsystem.theme.red700
 import com.cherrish.android.core.designsystem.type.CherrishSelectionChipStyle
 
 @Composable
@@ -55,11 +56,12 @@ fun CherrishSelectionChip(
     }
 
     val paddingValues = when (style) {
-        CherrishSelectionChipStyle.SELECTIONCHIP -> PaddingValues(10.dp)
+        CherrishSelectionChipStyle.SELECTIONCHIP -> PaddingValues(
+            horizontal = 10.dp,
+            vertical = 30.dp
+        )
         CherrishSelectionChipStyle.MISSIONCARD -> PaddingValues(horizontal = 7.dp, vertical = 6.dp)
     }
-
-    // 최소일떄 패딩
 
     Column(
         modifier = modifier
@@ -119,7 +121,9 @@ private fun CherrishMissionCardPreview() {
 
         CherrishSelectionChip(
             onClick = { selected = !selected },
-            modifier = Modifier.fillMaxWidth().padding(all = 30.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 30.dp),
             isSelected = selected,
             style = CherrishSelectionChipStyle.MISSIONCARD
         ) {
@@ -139,14 +143,15 @@ private fun CherrishMissionCardPreview() {
 
                 )
 
-                Image(
-                    painter = painterResource(
+                Icon(
+                    imageVector = ImageVector.vectorResource(
                         id = if (selected) {
                             R.drawable.ic_radiobtn_selected
                         } else {
                             R.drawable.ic_radiobtn_default
                         }
                     ),
+                    tint = if (selected) red700 else gray500,
                     contentDescription = null,
                     modifier = Modifier.align(Alignment.TopEnd)
                 )
