c31c41ce7abaa163b830c0bd13336bf7d370df41
sohee <144779368+sohee6989@users.noreply.github.com>
2026-01-10 11:00:39 +0900

    [REFACTOR/#21] 컴포넌트 세분화

---
+++ app/src/main/java/com/cherrish/android/presentation/home/component/DdayChip.kt
@@ -0,0 +1,31 @@
+package com.cherrish.android.presentation.home.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.cherrish.android.core.designsystem.theme.CherrishTheme
+
+@Composable
+fun DdayChip(
+    dDay: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .clip(shape = RoundedCornerShape(20.dp))
+            .background(color = CherrishTheme.colors.gray300)
+            .padding(horizontal = 10.dp)
+    ) {
+        Text(
+            text = dDay,
+            style = CherrishTheme.typography.body2R13,
+            color = CherrishTheme.colors.gray900
+        )
+    }
+}
---
+++ app/src/main/java/com/cherrish/android/presentation/home/component/MonthPlanContent.kt
@@ -0,0 +1,47 @@
+package com.cherrish.android.presentation.home.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.cherrish.android.core.designsystem.theme.CherrishTheme
+
+@Composable
+fun MonthPlanContent(
+    medicalProcedureNameDate: String,
+    medicalProcedureName: String,
+    dDay: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = medicalProcedureNameDate,
+            style = CherrishTheme.typography.title2M16,
+            color = CherrishTheme.colors.gray900
+        )
+
+        Text(
+            text = "•",
+            style = CherrishTheme.typography.title2M16,
+            color = CherrishTheme.colors.gray900
+        )
+
+        Text(
+            text = medicalProcedureName,
+            style = CherrishTheme.typography.title2SB16,
+            color = CherrishTheme.colors.gray900
+        )
+
+        DdayChip(
+            dDay = dDay
+        )
+    }
+}
