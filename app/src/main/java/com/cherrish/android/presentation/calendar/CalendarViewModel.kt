8be79541252284e935ee1742d8371826e0bb7717
Kim Nahyeon <nhyeonii@naver.com>
2026-01-10 17:10:48 +0900

    [FEAT/#26] CalendarViewModel 구현

---
+++ app/src/main/java/com/cherrish/android/presentation/calendar/CalendarViewModel.kt
@@ -0,0 +1,41 @@
+package com.cherrish.android.presentation.calendar
+
+import androidx.lifecycle.ViewModel
+import com.cherrish.android.core.common.extension.updateSuccess
+import com.cherrish.android.core.common.state.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import java.time.LocalDate
+import java.time.YearMonth
+import javax.inject.Inject
+
+@HiltViewModel
+class CalendarViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState = MutableStateFlow<UiState<CalendarUiState>>(
+        UiState.Success(CalendarUiState.FakeNormal)
+    )
+    val uiState: StateFlow<UiState<CalendarUiState>> = _uiState.asStateFlow()
+
+    fun onMonthChanged(yearMonth: YearMonth) {
+        _uiState.updateSuccess { currentState ->
+            currentState.copy(
+                selectedYearMonth = yearMonth,
+                selectedDate = null,
+                procedureInfoList = persistentListOf()
+            )
+        }
+    }
+
+    fun onDateClick(date: LocalDate) {
+
+    }
+
+    fun onEventClick(procedureId: Long) {
+
+    }
+
+}
