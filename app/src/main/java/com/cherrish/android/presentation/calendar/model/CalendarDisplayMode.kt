66346942c45e501114b888742fb8cd86f7d87dfe
Kim Nahyeon <nhyeonii@naver.com>
2026-01-11 02:31:50 +0900

    [FEAT/#16] @Immutable 추가

--- app/src/main/java/com/cherrish/android/presentation/calendar/model/CalendarDisplayMode.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/model/CalendarDisplayMode.kt
@@ -1,12 +1,15 @@
 package com.cherrish.android.presentation.calendar.model
 
+import androidx.compose.runtime.Immutable
 import java.time.LocalDate
 
 sealed interface CalendarDisplayMode {
+    @Immutable
     data class Normal(
         val procedureCountByDate: Map<LocalDate, Int>
     ) : CalendarDisplayMode
 
+    @Immutable
     data class Downtime(
         val downtimeByDate: Map<LocalDate, DownTimeStatus>,
         val selectedProcedureId: Long? = null
