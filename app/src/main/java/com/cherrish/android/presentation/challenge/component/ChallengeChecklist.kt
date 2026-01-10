129dbd18d27bf3b949a48110de1c380783a25865
sohee <144779368+sohee6989@users.noreply.github.com>
2026-01-10 11:46:05 +0900

    [CHORE/#19] 컴포넌트 파일 하나로 합치기

--- app/src/main/java/com/cherrish/android/presentation/challenge/component/ChallengeChecklist.kt
+++ app/src/main/java/com/cherrish/android/presentation/challenge/component/ChallengeChecklist.kt
@@ -1,22 +1,32 @@
 package com.cherrish.android.presentation.challenge.component
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cherrish.android.R
+import com.cherrish.android.core.common.extension.noRippleClickable
 import com.cherrish.android.core.designsystem.theme.CherrishTheme
 
 @Composable
@@ -52,6 +62,38 @@ fun ChallengeChecklist(
             checklistContent = checklistContent,
             contentColor = contentColor,
             contentDecoration = contentDecoration
+        )
+    }
+}
+
+@Composable
+private fun ChallengeChecklistItem(
+    @DrawableRes checklistIcon: Int,
+    onChecklistClick: () -> Unit,
+    checklistContent: String,
+    contentColor: Color,
+    contentDecoration: TextDecoration,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(6.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(id = checklistIcon),
+            contentDescription = null,
+            modifier = Modifier.noRippleClickable(
+                onClick = onChecklistClick
+            ),
+            tint = Color.Unspecified
+        )
+
+        Text(
+            text = checklistContent,
+            color = contentColor,
+            style = CherrishTheme.typography.body1R14,
+            textDecoration = contentDecoration
         )
     }
 }
