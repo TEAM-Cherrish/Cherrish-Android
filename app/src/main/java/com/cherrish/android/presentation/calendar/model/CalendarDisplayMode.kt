ce326de266ad750b00cd79bd10916173869e8159
Kim Nahyeon <nhyeonii@naver.com>
2026-01-10 17:10:26 +0900

    [REF/#26] CalendarDisplayMode 기본값 설정

--- app/src/main/java/com/cherrish/android/presentation/calendar/model/CalendarDisplayMode.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/model/CalendarDisplayMode.kt
@@ -1,14 +1,15 @@
 package com.cherrish.android.presentation.calendar.model
 
+import kotlinx.collections.immutable.persistentMapOf
 import java.time.LocalDate
 
 sealed interface CalendarDisplayMode {
     data class Normal(
-        val procedureCountByDate: Map<LocalDate, Int>
+        val procedureCountByDate: Map<LocalDate, Int> = persistentMapOf()
     ) : CalendarDisplayMode
 
     data class Downtime(
-        val downtimeByDate: Map<LocalDate, DownTimeStatus>,
+        val downtimeByDate: Map<LocalDate, DownTimeStatus> = persistentMapOf(),
         val selectedProcedureId: Long? = null
     ) : CalendarDisplayMode
 }
