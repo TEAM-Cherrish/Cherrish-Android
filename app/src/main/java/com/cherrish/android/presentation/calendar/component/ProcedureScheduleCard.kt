ac943c21101128712abaa98ab14983b42d14675a
Kim Nahyeon <nhyeonii@naver.com>
2026-01-12 17:14:24 +0900

    [REF/#16] 디자인 수정 사항 반영

--- app/src/main/java/com/cherrish/android/presentation/calendar/component/ProcedureScheduleCard.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/component/ProcedureScheduleCard.kt
@@ -17,9 +17,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -48,6 +46,13 @@ fun ProcedureScheduleCard(
     modifier: Modifier = Modifier
 ) {
     val listState = rememberLazyListState()
+
+    val isFirstItemVisible = remember {
+        derivedStateOf {
+            val firstVisibleItem = listState.layoutInfo.visibleItemsInfo.firstOrNull()
+            firstVisibleItem?.index == 0
+        }
+    }
 
     val isLastItemVisible = remember {
         derivedStateOf {
@@ -103,12 +108,32 @@ fun ProcedureScheduleCard(
             }
         }
 
+        if (!isFirstItemVisible.value) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .fillMaxWidth()
+                    .padding(top = 40.dp)
+                    .height(70.dp)
+                    .background(
+                        Brush.verticalGradient(
+                            colors = persistentListOf(
+                                CherrishTheme.colors.gray0,
+                                CherrishTheme.colors.gray0.copy(alpha = 0.8f),
+                                CherrishTheme.colors.gray0.copy(alpha = 0.5f),
+                                Color.Transparent
+                            )
+                        )
+                    )
+            )
+        }
+
         if (!isLastItemVisible.value) {
             Box(
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
                     .fillMaxWidth()
-                    .padding(top = 146.dp)
+                    .height(70.dp)
                     .background(
                         Brush.verticalGradient(
                             colors = persistentListOf(
