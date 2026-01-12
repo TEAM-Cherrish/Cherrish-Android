65dab9a2bfbc26e5d97e84e565a37f0e6ee26e0f
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-13 02:32:46 +0900

    [FEAT/#42] 루틴 챌린지 선택 컴포넌트 조립

--- app/src/main/java/com/cherrish/android/presentation/challenge/ChallengeScreen.kt
+++ app/src/main/java/com/cherrish/android/presentation/challenge/ChallengeScreen.kt
@@ -1,10 +1,22 @@
 package com.cherrish.android.presentation.challenge
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cherrish.android.presentation.challenge.component.ChallengeRoutineBottom
+import com.cherrish.android.presentation.challenge.component.ChallengeRoutineHeader
+import com.cherrish.android.presentation.challenge.component.ChallengeRoutineOnboardingBody
+import com.cherrish.android.presentation.challenge.model.ChallengeRoutineCategory
 
 @Composable
 fun ChallengeRoute(
@@ -18,8 +30,28 @@ private fun ChallengeScreen(
     paddingValues: PaddingValues,
     modifier: Modifier = Modifier
 ) {
-    Text(
-        "Challenge",
-        modifier = modifier.padding(paddingValues)
-    )
+    var selectedCategory by remember {
+        mutableStateOf<ChallengeRoutineCategory?>(value = null)
+    }
+
+    Column(modifier = modifier.padding(start = 26.dp, bottom = 64.dp, end = 26.dp)) {
+        ChallengeRoutineHeader()
+
+        Spacer(modifier = Modifier.height(70.dp))
+
+        ChallengeRoutineOnboardingBody(
+            selectedCategory = selectedCategory,
+            onCategoryClick = { selectedCategory = it }
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        ChallengeRoutineBottom()
+    }
+}
+
+@Preview
+@Composable
+private fun ChallengeScreenPreview() {
+    ChallengeScreen(paddingValues = PaddingValues(26.dp))
 }
