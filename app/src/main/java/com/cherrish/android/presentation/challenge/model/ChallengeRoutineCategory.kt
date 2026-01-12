022b3c7e8a9b6a545b687a1c32ff603bc41c0039
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-13 03:57:11 +0900

    [REFACTORE/#35] 코드 리뷰 반영

--- app/src/main/java/com/cherrish/android/presentation/challenge/model/ChallengeRoutineCategory.kt
+++ app/src/main/java/com/cherrish/android/presentation/challenge/model/ChallengeRoutineCategory.kt
@@ -3,6 +3,6 @@ package com.cherrish.android.presentation.challenge.model
 enum class ChallengeRoutineCategory(val label: String) {
     SKIN(label = "피부 컨디션"),
     LIFE(label = "생활 습관"),
-    BODY(label = "체형 관리 "),
+    BODY(label = "체형 관리"),
     WELLNESS(label = "웰니스 ∙ 마음 챙김")
 }
