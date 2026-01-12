9434fe40f4e76acf3c5356179407570c88452fa2
sssuhha <ysh83@sookmyung.ac.kr>
2026-01-13 03:04:07 +0900

    [REF/#40] 패딩 구현 방식 변경

--- app/src/main/java/com/cherrish/android/presentation/calendar/procedure/component/CautionDescription.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/procedure/component/CautionDescription.kt
@@ -1,9 +1,8 @@
 package com.cherrish.android.presentation.calendar.procedure.component
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,16 +15,14 @@ fun CautionDescription(
     modifier: Modifier = Modifier
 ) {
     Row(
-        modifier = modifier
-            .fillMaxWidth()
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
     ) {
         Text(
             text = "◎",
             style = CherrishTheme.typography.body3R12,
             color = CherrishTheme.colors.gray600
         )
-
-        Spacer(modifier = Modifier.size(4.dp))
 
         Text(
             text = "본 정보는 인터넷 빅데이터 검색 및 분석을 통해 " +
