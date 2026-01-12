e10b4849697cbd955f19d2d1f26ce9bf1315f081
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-12 22:08:10 +0900

    [FEAT/#35] 루틴 챌린지 그룹 추가

--- app/src/main/java/com/cherrish/android/presentation/challenge/component/ChallengeRoutineOnboardingBody.kt
+++ app/src/main/java/com/cherrish/android/presentation/challenge/component/ChallengeRoutineOnboardingBody.kt
@@ -1,10 +1,20 @@
 package com.cherrish.android.presentation.challenge.component
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cherrish.android.core.designsystem.component.chip.CherrishSelectionChip
 import com.cherrish.android.core.designsystem.theme.CherrishTheme
 
 @Composable
@@ -15,8 +25,51 @@ fun ChallengeRoutineOnboardingBody(modifier: Modifier= Modifier){
 
     }
 }
+@Composable
+private fun ChallengeRoutineSelectionChipGroup(modifier: Modifier = Modifier){
+    var isSelected by remember { mutableStateOf(value = false) } // 추후 viewmodel 생기면 변경 예정
+Column(modifier = modifier, verticalArrangement = Arrangement.spacedBy(space = 12.dp)){
+    Row(modifier = Modifier, horizontalArrangement = Arrangement.spacedBy(space = 12.dp)) {
+        CherrishSelectionChip(
+            text = "피부 컨디션",
+            onClick = { isSelected = !isSelected },
+            modifier = Modifier.weight(weight = 1f),
+            isSelected = isSelected
+        )
+        CherrishSelectionChip(
+            text = "생활 습관",
+            onClick = { isSelected = !isSelected },
+            modifier = Modifier.weight(weight = 1f),
+            isSelected = isSelected
+        )
+
+    }
+    Row(modifier = Modifier, horizontalArrangement = Arrangement.spacedBy(space = 12.dp)) {
+        CherrishSelectionChip(
+            text = "체형 관리",
+            onClick = { isSelected = !isSelected },
+            modifier = Modifier.weight(weight = 1f),
+            isSelected = isSelected
+        )
+        CherrishSelectionChip(
+            text = "웰니스 ∙ 마음 챙김",
+            onClick = { isSelected = !isSelected },
+            modifier = Modifier.weight(weight = 1f),
+            isSelected = isSelected
+        )
+
+    }
+}
+
+
+}
 @Preview(showBackground = true)
 @Composable
 private fun ChallengeRoutineOnboardingBodyPreview(){
     ChallengeRoutineOnboardingBody()
+}
+@Preview(showBackground = true)
+@Composable
+private fun ChallengeRoutineMissionCardGroupPreview(){
+    ChallengeRoutineSelectionChipGroup()
 }
