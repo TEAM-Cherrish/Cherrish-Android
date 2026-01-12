3dca479494e9f0b20e5afcebff0f1c366c103bce
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-12 21:00:07 +0900

    [FEAT/#35] 루틴 챌린지 선택 온보딩 바디 프리뷰 구현

--- app/src/main/java/com/cherrish/android/presentation/challenge/component/ChallengeRoutineOnboardingBody.kt
+++ app/src/main/java/com/cherrish/android/presentation/challenge/component/ChallengeRoutineOnboardingBody.kt
@@ -1,12 +1,22 @@
 package com.cherrish.android.presentation.challenge.component
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.cherrish.android.core.designsystem.theme.CherrishTheme
 
 @Composable
 fun ChallengeRoutineOnboardingBody(modifier: Modifier= Modifier){
-    Text(text ="지금 나에게 가장 필요한 관리 루틴을 선택하세요", modifier = Modifier, color = CherrishTheme.colors.gray1000,style = CherrishTheme.typography.title1SB18)
-    Text(text = "관리 루틴을 선택해주세요", modifier = Modifier, color = CherrishTheme.colors.gray1000, style = CherrishTheme.typography.title1SB18)
+    Column(modifier = modifier){
+        Text(text ="지금 나에게 가장 필요한 관리 루틴을 선택하세요", modifier = Modifier, color = CherrishTheme.colors.gray1000,style = CherrishTheme.typography.title1SB18)
+        Text(text = "관리 루틴을 선택해주세요", modifier = Modifier, color = CherrishTheme.colors.gray1000, style = CherrishTheme.typography.title1SB18)
+
+    }
+}
+@Preview(showBackground = true)
+@Composable
+private fun ChallengeRoutineOnboardingBodyPreview(){
+    ChallengeRoutineOnboardingBody()
 }
