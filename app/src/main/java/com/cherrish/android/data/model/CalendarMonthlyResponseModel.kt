fce0c02eed70e5611b77a6da2efbda010ef34bff
Kim Nahyeon <nhyeonii@naver.com>
2026-01-14 02:29:05 +0900

    [FEAT/#45] CalendarMonthlyResponseModel 구현

--- app/src/main/java/com/cherrish/android/data/model/CalendarMonthlyResponseModel.kt
+++ app/src/main/java/com/cherrish/android/data/model/CalendarMonthlyResponseModel.kt
@@ -1,2 +1,11 @@
 package com.cherrish.android.data.model
 
+import com.cherrish.android.data.remote.dto.response.CalendarMonthlyResponseDto
+
+data class CalendarMonthlyResponseModel(
+    val dailyProcedureCounts: Map<Int, Long>?
+)
+
+fun CalendarMonthlyResponseDto.toModel() = CalendarMonthlyResponseModel(
+    dailyProcedureCounts = this.dailyProcedureCounts
+)
