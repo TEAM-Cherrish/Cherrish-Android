aa400fd50230bc8d8a4b8a4244cf906a88a0fb70
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-14 02:31:28 +0900

    [FEAT/#48] MyPageHeader 구현 - MypageHeader 구현 - MypageHeader 프리뷰 구현

---
+++ app/src/main/java/com/cherrish/android/presentation/mypage/component/MypageHeader.kt
@@ -0,0 +1,64 @@
+package com.cherrish.android.presentation.mypage.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cherrish.android.R
+import com.cherrish.android.core.designsystem.theme.CherrishTheme
+
+@Composable
+fun MypageHeader(
+    profileIcon: Int, nicknameText: String, skinCareDay: Int, modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+
+        Icon(
+            imageVector = ImageVector.vectorResource(id = profileIcon),
+            contentDescription = "ProfileImage",
+            modifier = Modifier.clip(CircleShape),
+        )
+
+        Spacer(Modifier.width(width = 14.dp))
+
+        Column(modifier = Modifier.fillMaxWidth()) {
+
+            Text(
+                text = "안녕하세요, $nicknameText 님",
+                modifier = Modifier,
+                color = CherrishTheme.colors.gray1000,
+                style = CherrishTheme.typography.title1SB18
+            )
+
+            Text(
+                text = "관리 시작 D+ $skinCareDay",
+                modifier = Modifier,
+                color = CherrishTheme.colors.gray800,
+                style = CherrishTheme.typography.body1M14
+            )
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MyPageHeaderPreview() {
+    MypageHeader(
+        profileIcon = R.drawable.ic_launcher_foreground,
+        nicknameText = "김체체",
+        skinCareDay = 13
+    )
+}
