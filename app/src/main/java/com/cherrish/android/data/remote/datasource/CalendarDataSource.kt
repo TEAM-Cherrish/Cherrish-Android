f3736ec8f6290956a3688264e99205daec130b59
Kim Nahyeon <nhyeonii@naver.com>
2026-01-14 05:01:53 +0900

    [FEAT/#45] CalendarDataSource 구현

--- app/src/main/java/com/cherrish/android/data/remote/datasource/CalendarDataSource.kt
+++ app/src/main/java/com/cherrish/android/data/remote/datasource/CalendarDataSource.kt
@@ -1,8 +1,10 @@
 package com.cherrish.android.data.remote.datasource
 
 import com.cherrish.android.core.network.BaseResponse
+import com.cherrish.android.data.remote.dto.response.CalendarDailyResponseDto
 import com.cherrish.android.data.remote.dto.response.CalendarMonthlyResponseDto
 
 interface CalendarDataSource {
     suspend fun getCalendarMonthly(year: Int, month: Int): BaseResponse<CalendarMonthlyResponseDto>
+    suspend fun getCalendarDaily(date: String): BaseResponse<CalendarDailyResponseDto>
 }
--- app/src/main/java/com/cherrish/android/data/remote/datasourceimpl/CalendarDataSourceImpl.kt
+++ app/src/main/java/com/cherrish/android/data/remote/datasourceimpl/CalendarDataSourceImpl.kt
@@ -2,8 +2,10 @@ package com.cherrish.android.data.remote.datasourceimpl
 
 import com.cherrish.android.core.network.BaseResponse
 import com.cherrish.android.data.remote.datasource.CalendarDataSource
+import com.cherrish.android.data.remote.dto.response.CalendarDailyResponseDto
 import com.cherrish.android.data.remote.dto.response.CalendarMonthlyResponseDto
 import com.cherrish.android.data.remote.service.CalendarService
+import java.time.LocalDate
 import javax.inject.Inject
 
 class CalendarDataSourceImpl @Inject constructor(
@@ -14,4 +16,9 @@ class CalendarDataSourceImpl @Inject constructor(
         month: Int
     ): BaseResponse<CalendarMonthlyResponseDto> =
         calendarService.getCalendarMonthly(year = year, month = month)
+
+    override suspend fun getCalendarDaily(
+        date: String
+    ): BaseResponse<CalendarDailyResponseDto> =
+        calendarService.getCalendarDaily(date = date)
 }
