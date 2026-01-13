c6c4f4a4fd04b420316b9655c6f37637dcd5b403
sssuhha <ysh83@sookmyung.ac.kr>
2026-01-14 05:26:25 +0900

    [REF/#50] SelectionSection 수정

--- app/src/main/java/com/cherrish/android/presentation/calendar/procedure/content/ExistenceContent.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/procedure/content/ExistenceContent.kt
@@ -6,36 +6,34 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
-import com.cherrish.android.core.designsystem.component.section.CherrishSelectionSection
-import com.cherrish.android.core.designsystem.component.type.CherrishSectionChipType
 import com.cherrish.android.core.designsystem.theme.CherrishTheme
+import com.cherrish.android.presentation.calendar.procedure.component.SelectionSection
 import kotlinx.collections.immutable.persistentListOf
 
 @Composable
 fun ExistenceContent(
-    existence: Boolean,
     selectedIndex: Int?,
     onItemClick: (Int) -> Unit
 ) {
-    CherrishSelectionSection(
+    SelectionSection(
         title = "시술 일정을 추가해볼게요.",
         description = "시술을 선택하셨는지 확인할게요.",
         descriptionTextStyle = CherrishTheme.typography.body1R14,
         items = persistentListOf("선택한 시술이 있어요", "아직 선택 전이에요"),
         selectedIndex = selectedIndex,
-        onItemClick = onItemClick,
-        chipType = CherrishSectionChipType.SELECTION_CHIP
+        onItemClick = onItemClick
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun ExistenceContentPreview() {
-    var selectedIndex by remember { mutableIntStateOf(-1) }
+    CherrishTheme {
+        var selectedIndex by remember { mutableIntStateOf(-1) }
 
-    ExistenceContent(
-        existence = true,
-        selectedIndex = selectedIndex,
-        onItemClick = { selectedIndex = it }
-    )
+        ExistenceContent(
+            selectedIndex = selectedIndex,
+            onItemClick = { selectedIndex = it }
+        )
+    }
 }
