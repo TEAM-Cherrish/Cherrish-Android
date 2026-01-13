b849b042816c1e1ea6a1a37a6ad6a4b726ab3d53
Kim Nahyeon <nhyeonii@naver.com>
2026-01-14 05:02:07 +0900

    [FEAT/#45] CalendarService 구현

--- app/src/main/java/com/cherrish/android/data/remote/service/CalendarService.kt
+++ app/src/main/java/com/cherrish/android/data/remote/service/CalendarService.kt
@@ -1,6 +1,7 @@
 package com.cherrish.android.data.remote.service
 
 import com.cherrish.android.core.network.BaseResponse
+import com.cherrish.android.data.remote.dto.response.CalendarDailyResponseDto
 import com.cherrish.android.data.remote.dto.response.CalendarMonthlyResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,4 +12,9 @@ interface CalendarService {
         @Query("year") year: Int,
         @Query("month") month: Int
     ): BaseResponse<CalendarMonthlyResponseDto>
+
+    @GET("api/calendar/daily")
+    suspend fun getCalendarDaily(
+        @Query("date") date: String
+    ): BaseResponse<CalendarDailyResponseDto>
 }
