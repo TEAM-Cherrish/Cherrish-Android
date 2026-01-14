d7534311094dd7ea2ef1be4b4895adba68bead9b
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-14 21:32:06 +0900

    [FEAT/#57] ChallengeChipComponent 컴포넌트 구현
    - Todo 챌린지 컴포넌트 구현

---
+++ app/src/main/java/com/cherrish/android/presentation/challenge/component/ChallengeMissionCardChip.kt
@@ -0,0 +1,84 @@
+package com.cherrish.android.presentation.challenge.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cherrish.android.R
+import com.cherrish.android.core.common.extension.noRippleClickable
+import com.cherrish.android.core.designsystem.theme.CherrishTheme
+@Composable
+fun ChallengeMissionCardChip(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    isSelected: Boolean = false
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(shape = RoundedCornerShape(size = 10.dp))
+            .border(
+                width = 1.dp,
+                color = if (isSelected) CherrishTheme.colors.red500 else CherrishTheme.colors.gray500,
+                shape = RoundedCornerShape(size = 10.dp)
+            )
+            .background(
+             color = if (isSelected) CherrishTheme.colors.red100 else CherrishTheme.colors.gray0
+            )
+            .padding(top= 14.dp, start = 14.dp, bottom = 14.dp, end = 44.dp)
+            .noRippleClickable(onClick = onClick),
+
+        horizontalArrangement = Arrangement.spacedBy(space = 6.dp),
+
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Icon(
+            imageVector = ImageVector.vectorResource(
+                id = if (isSelected) R.drawable.ic_radiobtn_selected else R.drawable.ic_radiobtn_default
+            ),
+            tint = if (isSelected) CherrishTheme.colors.red700 else CherrishTheme.colors.gray500,
+            contentDescription = null,
+        )
+
+        Text(
+            text = text,
+            style = CherrishTheme.typography.body1R14,
+            color = if (isSelected) CherrishTheme.colors.gray800 else CherrishTheme.colors.gray700,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ChallengeMissionCardChipPreview() {
+    CherrishTheme {
+        var selected by remember { mutableStateOf(false) }
+
+        ChallengeMissionCardChip(
+            text = "선크림 3번 바르기",
+            isSelected = selected,
+            onClick = { selected = !selected }
+        )
+    }
+}
