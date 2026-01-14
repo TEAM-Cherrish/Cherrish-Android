8967d5b202161195a66fb722192f105abfd11dbe
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-15 02:23:07 +0900

    [FEAT/#59] MyPageInfo Model 클래스 구현
    - MyPageInfo Model 클래스 구현
    - companion object로 Fake data 구현

---
+++ app/src/main/java/com/cherrish/android/presentation/mypage/model/MyPageInfo.kt
@@ -0,0 +1,9 @@
+package com.cherrish.android.presentation.mypage.model
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+data class MyPageInfo(
+    val nicknameText: String,
+    val skinCareDay: Int
+)
