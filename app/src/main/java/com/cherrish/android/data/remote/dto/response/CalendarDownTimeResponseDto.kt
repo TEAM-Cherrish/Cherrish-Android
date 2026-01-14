04b095cdd2dc2e6d66530a6bfa90977b883d9c64
Kim Nahyeon <nhyeonii@naver.com>
2026-01-14 15:24:39 +0900

    [FEAT/#45] CalendarDownTImeResponseDto 구현

---
+++ app/src/main/java/com/cherrish/android/data/remote/dto/response/CalendarDownTimeResponseDto.kt
@@ -0,0 +1,18 @@
+package com.cherrish.android.data.remote.dto.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CalendarDownTimeResponseDto(
+    @SerialName("userProcedureId")
+    val userProcedureId: Long,
+    @SerialName("scheduleAt")
+    val scheduleAt: String,
+    @SerialName("sensitiveDays")
+    val sensitiveDays: List<String>,
+    @SerialName("cautionDays")
+    val cautionDays: List<String>,
+    @SerialName("downtimeDays")
+    val downtimeDays: List<String>
+)
