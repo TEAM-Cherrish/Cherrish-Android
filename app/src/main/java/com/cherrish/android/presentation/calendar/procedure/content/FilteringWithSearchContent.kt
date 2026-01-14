b8ece82d72af7acec7b6c47ea68c890800f94b74
sssuhha <ysh83@sookmyung.ac.kr>
2026-01-15 02:39:01 +0900

    [FEAT/#50] FilteringWithSearchContent 구현
    - 시술플로우 시술 필터링 + 검색 뷰 컨텐트 구현

---
+++ app/src/main/java/com/cherrish/android/presentation/calendar/procedure/content/FilteringWithSearchContent.kt
@@ -0,0 +1,153 @@
+package com.cherrish.android.presentation.calendar.procedure.content
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cherrish.android.core.designsystem.theme.CherrishTheme
+import com.cherrish.android.presentation.calendar.component.ProcedureTextField
+import com.cherrish.android.presentation.calendar.procedure.component.CautionDescription
+import com.cherrish.android.presentation.calendar.procedure.component.ProcedureCard
+import com.cherrish.android.presentation.calendar.procedure.model.ProcedureCardDisplayMode
+import com.cherrish.android.presentation.calendar.procedure.model.ProcedureCardItemUiModel
+
+/* TODO: 삭제 예정 */
+private val mockProcedureCardItems = listOf(
+    ProcedureCardItemUiModel(
+        id = 1L,
+        name = "레이저 토닝",
+        category = "색소 개선 | 토닝",
+        minDowntimeDays = 3,
+        maxDowntimeDays = 5,
+        displayMode = ProcedureCardDisplayMode.Basic
+    ),
+    ProcedureCardItemUiModel(
+        id = 2L,
+        name = "레이저 토닝",
+        category = "색소 개선 | 토닝",
+        minDowntimeDays = 2,
+        maxDowntimeDays = 3,
+        displayMode = ProcedureCardDisplayMode.Basic
+    ),
+    ProcedureCardItemUiModel(
+        id = 3L,
+        name = "레이저 토닝",
+        category = "색소 개선 | 토닝",
+        minDowntimeDays = 5,
+        maxDowntimeDays = 8,
+        displayMode = ProcedureCardDisplayMode.Basic
+    ),
+    ProcedureCardItemUiModel(
+        id = 4L,
+        name = "레이저 토닝",
+        category = "색소 개선 | 토닝",
+        minDowntimeDays = 3,
+        maxDowntimeDays = 5,
+        displayMode = ProcedureCardDisplayMode.Basic
+    ),
+    ProcedureCardItemUiModel(
+        id = 5L,
+        name = "레이저 토닝",
+        category = "색소 개선 | 토닝",
+        minDowntimeDays = 3,
+        maxDowntimeDays = 5,
+        displayMode = ProcedureCardDisplayMode.Basic
+    ),
+    ProcedureCardItemUiModel(
+        id = 6L,
+        name = "레이저 토닝",
+        category = "색소 개선 | 토닝",
+        minDowntimeDays = 3,
+        maxDowntimeDays = 5,
+        displayMode = ProcedureCardDisplayMode.Basic
+    ),
+    ProcedureCardItemUiModel(
+        id = 7L,
+        name = "레이저 토닝",
+        category = "색소 개선 | 토닝",
+        minDowntimeDays = 3,
+        maxDowntimeDays = 5,
+        displayMode = ProcedureCardDisplayMode.Basic
+    )
+)
+
+@Composable
+fun FilteringWithSearchContent(
+    cardItems: List<ProcedureCardItemUiModel>,
+    selectedCardId: Long?,
+    onCardClick: (Long) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var text by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.fillMaxWidth()
+            .padding(horizontal = 24.dp)
+    ) {
+        ProcedureTextField(
+            value = text,
+            onValueChange = { text = it },
+            onSearchAction = {},
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        LazyColumn(
+            modifier = modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            contentPadding = PaddingValues(bottom = 20.dp)
+        ) {
+            item {
+                CautionDescription()
+            }
+
+            item {
+                Spacer(modifier = Modifier.height(10.dp))
+            }
+
+            itemsIndexed(
+                items = cardItems,
+                key = { _, item -> item.id }
+            ) { _, item ->
+                ProcedureCard(
+                    name = item.name,
+                    category = item.category,
+                    minDowntimeDays = item.minDowntimeDays,
+                    maxDowntimeDays = item.maxDowntimeDays,
+                    onCardClick = { onCardClick(item.id) },
+                    isSelected = selectedCardId == item.id,
+                    displayMode = item.displayMode
+
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+private fun FilteringWithSearchContentPreview() {
+    CherrishTheme {
+        var selectedCardId by remember { mutableStateOf<Long?>(null) }
+
+        FilteringWithSearchContent(
+            cardItems = mockProcedureCardItems,
+            selectedCardId = selectedCardId,
+            onCardClick = { clickedId ->
+                selectedCardId = if (selectedCardId == clickedId) null else clickedId
+            }
+        )
+    }
+}
