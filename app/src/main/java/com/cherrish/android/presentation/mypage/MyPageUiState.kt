5969f69469d887d18d0d25c874fbc2ec1da6a1a2
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-15 02:25:18 +0900

    [FEAT/#59] MyPageUiState 구현
    - companion object로 Fake data 구현

---
+++ app/src/main/java/com/cherrish/android/presentation/mypage/MyPageUiState.kt
@@ -0,0 +1,21 @@
+package com.cherrish.android.presentation.mypage
+
+import androidx.compose.runtime.Immutable
+import com.cherrish.android.presentation.mypage.model.MyPageInfo
+import kotlinx.collections.immutable.persistentListOf
+
+@Immutable
+data class MyPageUiState(
+    val nicknameText: String = "",
+    val skinCareDay: Int = 0
+) {
+    companion object {
+        val Users = persistentListOf({
+            MyPageInfo(
+                nicknameText = "홍길동",
+                skinCareDay = 3
+            )
+
+        })
+    }
+}
