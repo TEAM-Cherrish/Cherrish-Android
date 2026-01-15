bfd4b9bf1ebd89737677485657ef6d6b7da55763
Kim Nahyeon <nhyeonii@naver.com>
2026-01-16 02:14:05 +0900

    [FEAT/#65] import 추가

--- app/src/main/java/com/cherrish/android/presentation/calendar/component/ProcedureScheduleCard.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/component/ProcedureScheduleCard.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
