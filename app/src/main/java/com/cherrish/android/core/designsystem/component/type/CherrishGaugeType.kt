2c1be3d06c1503906e615ae564c686053e56fc97
sohee <144779368+sohee6989@users.noreply.github.com>
2026-01-15 21:59:16 +0900

    [CHORE/#21] CherrishGaugeType에 percent 변수 추가

--- app/src/main/java/com/cherrish/android/core/designsystem/component/type/CherrishGaugeType.kt
+++ app/src/main/java/com/cherrish/android/core/designsystem/component/type/CherrishGaugeType.kt
@@ -1,10 +1,11 @@
 package com.cherrish.android.core.designsystem.component.type
 
 enum class CherrishGaugeType(
-    val step: Int
+    val step: Int,
+    val percent: Int
 ) {
-    LEVEL1(1),
-    LEVEL2(2),
-    LEVEL3(3),
-    LEVEL4(4)
+    LEVEL1(step = 1, percent = 25),
+    LEVEL2(step = 2, percent = 50),
+    LEVEL3(step = 3, percent = 75),
+    LEVEL4(step = 4, percent = 100)
 }
