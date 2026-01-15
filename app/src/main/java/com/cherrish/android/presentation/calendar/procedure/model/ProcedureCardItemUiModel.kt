beebe2f77ba4247c3bed9f16fc4e9d19e45f3bed
sssuhha <ysh83@sookmyung.ac.kr>
2026-01-15 21:06:37 +0900

    [REF/#50] UiModel에 @Immutable 어노테이션 추가

--- app/src/main/java/com/cherrish/android/presentation/calendar/procedure/model/ProcedureCardItemUiModel.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/procedure/model/ProcedureCardItemUiModel.kt
@@ -1,5 +1,8 @@
 package com.cherrish.android.presentation.calendar.procedure.model
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class ProcedureCardItemUiModel(
     val id: Long,
     val name: String,
--- app/src/main/java/com/cherrish/android/presentation/calendar/procedure/model/ProcedureWorryUiModel.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/procedure/model/ProcedureWorryUiModel.kt
@@ -1,5 +1,8 @@
 package com.cherrish.android.presentation.calendar.procedure.model
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class ProcedureWorryUiModel(
     val id: Long,
     val content: String
