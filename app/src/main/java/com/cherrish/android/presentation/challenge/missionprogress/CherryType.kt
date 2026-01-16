dd9c4e4cd4f89bdccfbadf9323740ed0a94f7372
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-17 00:39:24 +0900

    [FEAT/#73] CherryType 선언

---
+++ app/src/main/java/com/cherrish/android/presentation/challenge/missionprogress/CherryType.kt
@@ -0,0 +1,42 @@
+package com.cherrish.android.presentation.challenge.missionprogress
+
+import androidx.annotation.DrawableRes
+import com.cherrish.android.R
+
+enum class CherryType (
+    val step : Int,
+    val level : Int,
+    val stageName : String,
+@DrawableRes val imageRes: Int
+){
+    MONGRONG(
+        step = 0,
+        level = 0,
+        stageName = "몽롱체리",
+        imageRes = R.drawable.img_lv0
+    ),
+    PPODUK(
+        step = 1,
+        level = 1,
+        stageName = "뽀득체리",
+        imageRes = R.drawable.img_lv1
+    ),
+    CHOKCHOK(
+        step = 2,
+        level = 2,
+        stageName = "촉촉체리",
+        imageRes = R.drawable.img_lv2
+    ),
+    BBANGBBANG(
+        step = 3,
+        level = 3,
+        stageName = "팡팡체리",
+        imageRes = R.drawable.img_lv3
+    ),
+    KKUKKU(
+        step = 4,
+        level = 4,
+        stageName = "꾸꾸체리",
+        imageRes = R.drawable.img_lv4
+    ),
+}
