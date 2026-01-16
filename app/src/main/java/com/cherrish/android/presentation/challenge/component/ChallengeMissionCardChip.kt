ee47bc270c42a5d4feaf94009112e1e21ef9b287
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-16 17:47:31 +0900

    [REF/#35] ChallengeMissionCardChip 수정 - ChallengeMissionCardChip LazyColumn으로 수정

--- app/src/main/java/com/cherrish/android/presentation/challenge/component/ChallengeMissionCardChip.kt
+++ app/src/main/java/com/cherrish/android/presentation/challenge/component/ChallengeMissionCardChip.kt
@@ -42,7 +42,7 @@ fun ChallengeMissionCardChip(
             .border(
                 width = 1.dp,
                 color =
-                if (isSelected) CherrishTheme.colors.red500 else CherrishTheme.colors.gray500,
+                    if (isSelected) CherrishTheme.colors.red500 else CherrishTheme.colors.gray500,
                 shape = RoundedCornerShape(size = 10.dp)
             )
             .background(
@@ -51,7 +51,7 @@ fun ChallengeMissionCardChip(
             .padding(vertical = 14.dp)
             .padding(start = 14.dp, end = 44.dp)
             .noRippleClickable(onClick = onClick),
-        horizontalArrangement = Arrangement.spacedBy(space = 6.dp),
+        horizontalArrangement = Arrangement.spacedBy(space = 8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
@@ -78,12 +78,12 @@ fun ChallengeMissionCardChip(
 @Composable
 private fun ChallengeMissionCardChipPreview() {
     CherrishTheme {
-        var selected by remember { mutableStateOf(value = false) }
+        var isSelected by remember { mutableStateOf(value = false) }
 
         ChallengeMissionCardChip(
             text = "선크림 3번 바르기",
-            isSelected = selected,
-            onClick = { selected = !selected }
+            isSelected = isSelected,
+            onClick = { isSelected = !isSelected }
         )
     }
 }
