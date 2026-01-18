c3b211bcc3d90ceb29994f1e51a4aaac8f681361
sohee <144779368+sohee6989@users.noreply.github.com>
2026-01-19 00:27:02 +0900

    [CHORE/#25] 파일명 수정

--- app/src/main/java/com/cherrish/android/presentation/onboarding/model/OnboardingCherryType.kt
+++ app/src/main/java/com/cherrish/android/presentation/onboarding/model/OnboardingCherryType.kt
@@ -7,9 +7,9 @@ enum class OnboardingCherryType(
     @DrawableRes val image: Int,
     val index: Int
 ) {
-    LV0(image = R.drawable.img_lv0, index = 0),
-    LV1(image = R.drawable.img_lv1, index = 1),
-    LV2(image = R.drawable.img_lv2, index = 2),
-    LV3(image = R.drawable.img_lv3, index = 3),
-    LV4(image = R.drawable.img_lv4, index = 4)
+    LV0(image = R.drawable.img_onboarding_lv0, index = 0),
+    LV1(image = R.drawable.img_onboarding_lv1, index = 1),
+    LV2(image = R.drawable.img_onboarding_lv2, index = 2),
+    LV3(image = R.drawable.img_onboarding_lv3, index = 3),
+    LV4(image = R.drawable.img_onboarding_lv4, index = 4)
 }
