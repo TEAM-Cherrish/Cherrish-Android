5c4e32dae1f16ce2fa24cd6837aecd58495ad180
sohee <144779368+sohee6989@users.noreply.github.com>
2026-01-19 00:01:28 +0900

    [CHORE/#76] 버튼 enable 분기 추가

--- app/src/main/java/com/cherrish/android/presentation/onboarding/information/InformationUiState.kt
+++ app/src/main/java/com/cherrish/android/presentation/onboarding/information/InformationUiState.kt
@@ -8,7 +8,10 @@ data class InformationUiState(
     val age: String = ""
 ) {
     val buttonEnabled: Boolean
-        get() = username.isNotBlank() && age.toIntOrNull()?.let { it > 0 } == true
+        get() {
+            val ageInt = age.toIntOrNull() ?: return false
+            return username.isNotBlank() && age.toIntOrNull()?.let { it > 0 } == true && ageInt in 1..100 && username.length <= 7
+        }
 }
 
 sealed interface InformationSideEffect {
