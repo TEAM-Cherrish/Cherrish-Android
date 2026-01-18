8de2a7bb5c4c3979906c539cf932e3a7dc143e95
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-18 21:14:57 +0900

    [CHORE/#35] ktLint 실행

--- app/src/main/java/com/cherrish/android/presentation/challenge/mission/model/ChallengeMissionModel.kt
+++ app/src/main/java/com/cherrish/android/presentation/challenge/mission/model/ChallengeMissionModel.kt
@@ -1,5 +1,8 @@
 package com.cherrish.android.presentation.challenge.mission.model
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class ChallengeMissionModel(
     val id: Int,
     val missionContent: String
