44c9ab669cd5aca651e11d8a4b287ca1c9dec7ae
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-19 02:11:55 +0900

    [FEAT/#83] StartScreen 화면 구현

---
+++ app/src/main/java/com/cherrish/android/presentation/challenge/ChallengeStartScreen.kt
@@ -0,0 +1,115 @@
+package com.cherrish.android.presentation.challenge
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cherrish.android.R
+import com.cherrish.android.core.common.extension.noRippleClickable
+import com.cherrish.android.core.designsystem.component.button.CherrishButton
+import com.cherrish.android.core.designsystem.theme.CherrishTheme
+
+@Composable
+fun ChallengeStartRoute(paddingValues: PaddingValues) {
+    ChallengeStartScreen(paddingValues = paddingValues)
+}
+
+@Composable
+private fun ChallengeStartScreen(
+    paddingValues: PaddingValues,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier
+        .fillMaxSize()
+        .padding(paddingValues)
+    ) {
+
+        Spacer(modifier = Modifier.weight(98f))
+
+        ChallengeStartTitle(modifier = Modifier.fillMaxWidth())
+
+        Spacer(Modifier.weight(20f))
+
+        Image(
+            painter = painterResource(id = R.drawable.img_challenge_start),
+            contentDescription = null,
+            modifier = Modifier.weight(364f)
+        )
+
+        Spacer(Modifier.weight(18f))
+
+        ChallengeStartInfo()
+
+        Spacer(Modifier.weight(12f))
+
+        CherrishButton(
+            text = "다음",
+            onClick = {}
+        )
+
+        Spacer(modifier = Modifier.weight(34f))
+
+    }
+}
+
+@Composable
+private fun ChallengeStartInfo(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_challenge_info),
+            contentDescription = null,
+            tint = CherrishTheme.colors.gray600,
+        )
+
+        Text(
+            text = "이 챌린지는 설정 시점부터 7일간 진행됩니다.",
+            color = CherrishTheme.colors.gray600,
+            style = CherrishTheme.typography.body3M12
+        )
+    }
+}
+
+@Composable
+private fun ChallengeStartTitle(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            text = "루틴을 지킬수록 체리가 성장해요.",
+            color = CherrishTheme.colors.gray1000,
+            style = CherrishTheme.typography.headlineSB20
+        )
+
+        Text(
+            text = "루틴을 지킬수록 체리가 성장해요.",
+            color = CherrishTheme.colors.gray800,
+            style = CherrishTheme.typography.title2M16
+        )
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ChallengeStartScreenPreview() {
+    ChallengeStartScreen(paddingValues = PaddingValues(horizontal = 17.dp))
+}
