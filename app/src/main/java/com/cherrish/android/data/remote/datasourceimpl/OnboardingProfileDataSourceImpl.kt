34ff267b84f853fc6241506634b1eb74bd061d7b
sohee <144779368+sohee6989@users.noreply.github.com>
2026-01-18 23:11:46 +0900

    [FEAT/#76] DataSource 생성

---
+++ app/src/main/java/com/cherrish/android/data/remote/datasourceimpl/OnboardingProfileDataSourceImpl.kt
@@ -0,0 +1,16 @@
+package com.cherrish.android.data.remote.datasourceimpl
+
+import com.cherrish.android.core.network.BaseResponse
+import com.cherrish.android.data.remote.datasource.OnboardingProfileDataSource
+import com.cherrish.android.data.remote.dto.request.OnboardingProfileRequestDto
+import com.cherrish.android.data.remote.dto.response.OnboardingProfileResponseDto
+import com.cherrish.android.data.remote.service.OnboardingProfileService
+import javax.inject.Inject
+
+class OnboardingProfileDataSourceImpl @Inject constructor(
+    private val onboardingProfileService: OnboardingProfileService
+) : OnboardingProfileDataSource {
+    override suspend fun postOnboardingProfile(request: OnboardingProfileRequestDto): BaseResponse<OnboardingProfileResponseDto> {
+        return onboardingProfileService.postOnboardingProfile(request)
+    }
+}
