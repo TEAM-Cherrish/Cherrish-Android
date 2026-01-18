75793c987c8d0d6dd1d25a43870f2fb25d390a6f
sohee <144779368+sohee6989@users.noreply.github.com>
2026-01-18 23:08:05 +0900

    [FEAT/#76] ImePadding 이중 패딩 관련 modifier 확장함수 추가

--- app/src/main/java/com/cherrish/android/core/common/extension/ModifierExt.kt
+++ app/src/main/java/com/cherrish/android/core/common/extension/ModifierExt.kt
@@ -4,8 +4,14 @@ import android.graphics.BlurMaskFilter
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawBehind
@@ -17,6 +23,9 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.drawOutline
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.findRootCoordinates
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -85,4 +94,16 @@ fun Modifier.dropShadow(
             canvas.restore()
         }
     }
+}
+
+fun Modifier.advancedImePadding() = composed {
+    var consumePadding by remember { mutableStateOf(0) }
+    onGloballyPositioned { coordinates ->
+        consumePadding = coordinates.findRootCoordinates().size.height -
+                (coordinates.positionInWindow().y + coordinates.size.height).toInt().coerceAtLeast(0)
+    }
+        .consumeWindowInsets(
+            PaddingValues(bottom = with(LocalDensity.current) { consumePadding.toDp() })
+        )
+        .imePadding()
 }
