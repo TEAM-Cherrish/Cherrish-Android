d0596ae12256f35f78c4a9c48a1b94fa07ef9b65
sssuhha <ysh83@sookmyung.ac.kr>
2026-01-19 03:46:06 +0900

    [REF/#67] 데이터 클래스 ImmutableList로 변경

--- app/src/main/java/com/cherrish/android/presentation/calendar/procedure/model/ProcedureWithDowntime.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/procedure/model/ProcedureWithDowntime.kt
@@ -1,5 +1,8 @@
 package com.cherrish.android.presentation.calendar.procedure.model
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class ProcedureWithDowntime(
     val procedureId: Long,
     val downtimeDays: Int
