31fff1d4141fad078ab3d14c0f929983c054c6ef
Kim Nahyeon <nhyeonii@naver.com>
2026-01-20 04:40:23 +0900

    [REF/#100] 다운타임 모드일시 월 변경 시에도 다운타임 모드 적용

--- app/src/main/java/com/cherrish/android/presentation/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/CalendarViewModel.kt
@@ -81,13 +81,30 @@ class CalendarViewModel @Inject constructor(
     }
 
     fun onMonthChange(yearMonth: YearMonth) {
-        val newSelectedDate = if (yearMonth == YearMonth.now()) {
-            LocalDate.now()
-        } else {
-            yearMonth.atDay(1)
-        }
+        _uiState.updateSuccess { currentState ->
+            val currentMode = currentState.calendarDisplayMode
 
-        loadMonthlyCalendar(yearMonth, newSelectedDate)
+            val newSelectedDate = if (yearMonth == YearMonth.now()) {
+                LocalDate.now()
+            } else {
+                yearMonth.atDay(1)
+            }
+
+            if (currentMode is CalendarDisplayMode.Downtime) {
+                _uiState.update {
+                    UiState.Success(
+                        currentState.copy(
+                            selectedYearMonth = yearMonth,
+                            selectedDate = newSelectedDate
+                        )
+                    )
+                }
+            } else {
+                loadMonthlyCalendar(yearMonth, newSelectedDate)
+            }
+
+            currentState
+        }
     }
 
     fun onEventClick(procedureId: Long) {
