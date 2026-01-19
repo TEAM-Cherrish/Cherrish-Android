7d2fba7a7735e7d80300fee65026c46cc8fba9d3
sohee <144779368+sohee6989@users.noreply.github.com>
2026-01-20 00:15:29 +0900

    [CHORE/#76] ktlint 체크

--- app/src/main/java/com/cherrish/android/core/common/extension/ModifierExt.kt
+++ app/src/main/java/com/cherrish/android/core/common/extension/ModifierExt.kt
@@ -99,8 +99,10 @@ fun Modifier.dropShadow(
 fun Modifier.advancedImePadding() = composed {
     var consumePadding by remember { mutableStateOf(0) }
     onGloballyPositioned { coordinates ->
-        consumePadding = (coordinates.findRootCoordinates().size.height -
-                (coordinates.positionInWindow().y + coordinates.size.height).toInt()).coerceAtLeast(
+        consumePadding = (
+            coordinates.findRootCoordinates().size.height -
+                (coordinates.positionInWindow().y + coordinates.size.height).toInt()
+            ).coerceAtLeast(
             0
         )
     }
