f302f5422ae1b4de2ca147bd5a566e7df15c5494
sohee <144779368+sohee6989@users.noreply.github.com>
2026-01-20 23:37:33 +0900

    [FEAT/#109] ChallengeRoutineUiModel 생성

---
+++ app/src/main/java/com/cherrish/android/presentation/challenge/missionprogress/model/ChallengeRoutineUiModel.kt
@@ -0,0 +1,7 @@
+package com.cherrish.android.presentation.challenge.missionprogress.model
+
+data class ChallengeRoutineUiModel(
+    val routineId: Long,
+    val routineName: String,
+    val isCompleted: Boolean
+)
