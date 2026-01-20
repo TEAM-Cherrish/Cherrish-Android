560a01832d156b1bacfc3e1fa56e64cb4b94baaf
sohee <144779368+sohee6989@users.noreply.github.com>
2026-01-20 20:39:19 +0900

    [FEAT/#109] 루틴 완료 service 생성

--- app/src/main/java/com/cherrish/android/data/remote/service/ChallengeMissionService.kt
+++ app/src/main/java/com/cherrish/android/data/remote/service/ChallengeMissionService.kt
@@ -3,8 +3,16 @@ package com.cherrish.android.data.remote.service
 import com.cherrish.android.core.network.BaseResponse
 import com.cherrish.android.data.remote.dto.response.ChallengeMissionResponseDto
 import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ChallengeMissionService {
     @GET("api/demo/challenges")
     fun getChallengeMissions(): BaseResponse<ChallengeMissionResponseDto>
+
+    @PATCH("api/demo/challenges/routines/{routineId}/toggle")
+    fun patchChallengeRoutinesComplete(
+        @Path("routineId") routineId: Long
+    )
 }
