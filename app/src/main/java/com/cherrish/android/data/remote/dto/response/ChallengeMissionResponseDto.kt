4ad31ab931cf8a306a58f3869fe73b01c8e37597
sohee <144779368+sohee6989@users.noreply.github.com>
2026-01-20 20:39:14 +0900

    [FEAT/#109] 루틴 완료 responseDto 생성

--- app/src/main/java/com/cherrish/android/data/remote/dto/response/ChallengeMissionResponseDto.kt
+++ app/src/main/java/com/cherrish/android/data/remote/dto/response/ChallengeMissionResponseDto.kt
@@ -7,8 +7,34 @@ import kotlinx.serialization.Serializable
 data class ChallengeMissionResponseDto(
     @SerialName("challengeId")
     val challengeId: Long,
-    @SerialName("challengeName")
-    val challengeName: String
+    @SerialName("title")
+    val title: String,
+    @SerialName("currentDay")
+    val currentDay: Int,
+    @SerialName("progressPercentage")
+    val progressPercentage: Int,
+    @SerialName("cherryLevel")
+    val cherryLevel: Int,
+    @SerialName("cherryLevelName")
+    val cherryLevelName: String,
+    @SerialName("progressToNextLevel")
+    val progressToNextLevel: Double,
+    @SerialName("remainingRoutinesToNextLevel")
+    val remainingRoutinesToNextLevel: Int,
+    @SerialName("routines")
+    val routines: List<ChallengeRoutineResponseDto>,
+    @SerialName("cheeringMessage")
+    val cheeringMessage: String
+)
 
-
+@Serializable
+data class ChallengeRoutineResponseDto(
+    @SerialName("routineId")
+    val routineId: Long,
+    @SerialName("name")
+    val name: String,
+    @SerialName("scheduledDate")
+    val scheduledDate: String,
+    @SerialName("isComplete")
+    val isComplete: Boolean
 )
