88516658d18eaca1bf76b32e1d8b754f6b64635e
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-20 22:40:55 +0900

    [FEAT/#103] routineUiModel로 변경
    기존 RoutineUiModel과 데이터 model를 분리하기 위해 따로 구현

---
+++ app/src/main/java/com/cherrish/android/presentation/challenge/routine/model/ChallengeRoutineUiModel.kt
@@ -0,0 +1,17 @@
+package com.cherrish.android.presentation.challenge.routine.model
+
+import androidx.compose.runtime.Immutable
+import com.cherrish.android.data.model.ChallengeHomecareRoutinesResponseModel
+
+@Immutable
+data class ChallengeRoutineUiModel(
+    val id: Int,
+    val routine: String,
+    val isSelected: Boolean = false
+)
+
+fun ChallengeHomecareRoutinesResponseModel.toUiModel() =
+    ChallengeRoutineUiModel(
+        id = id,
+        routine = description
+    )
