ad3ae642a10e1975d3296225bb05435dc86f6403
sssuhha <ysh83@sookmyung.ac.kr>
2026-01-21 04:08:23 +0900

    [REF/#90] startDate 타입 원상복구

--- app/src/main/java/com/cherrish/android/presentation/calendar/navigation/CalendarNavigator.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/navigation/CalendarNavigator.kt
@@ -8,7 +8,6 @@ import androidx.navigation.compose.composable
 import com.cherrish.android.core.common.navigation.MainTabRoute
 import com.cherrish.android.presentation.calendar.CalendarRoute
 import com.cherrish.android.presentation.calendar.procedure.ProcedureRoute
-import kotlinx.serialization.Contextual
 import java.time.LocalDate
 import kotlinx.serialization.Serializable
 
@@ -16,13 +15,13 @@ import kotlinx.serialization.Serializable
 data object Calendar : MainTabRoute
 
 @Serializable
-data class Procedure(@Contextual val startDate: LocalDate)
+data class Procedure(val startDate: String)
 
 fun NavController.navigateToCalendar(navOptions: NavOptions? = null) =
     navigate(Calendar, navOptions)
 
 fun NavController.navigateToProcedure(startDate: LocalDate, navOptions: NavOptions? = null) =
-    navigate(Procedure(startDate = startDate), navOptions)
+    navigate(Procedure(startDate = startDate.toString()), navOptions)
 
 fun NavGraphBuilder.calendarNavGraph(
     paddingValues: PaddingValues,
--- app/src/main/java/com/cherrish/android/presentation/calendar/procedure/ProcedureViewModel.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/procedure/ProcedureViewModel.kt
@@ -3,6 +3,7 @@ package com.cherrish.android.presentation.calendar.procedure
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.toRoute
 import com.cherrish.android.core.common.extension.onLogFailure
 import com.cherrish.android.core.common.extension.updateSuccess
 import com.cherrish.android.core.common.state.UiState
@@ -14,6 +15,7 @@ import com.cherrish.android.data.repository.UserProcedureRepository
 import com.cherrish.android.data.repository.WorryRepository
 import com.cherrish.android.presentation.calendar.CalendarEvent
 import com.cherrish.android.presentation.calendar.CalendarRefreshEventBus
+import com.cherrish.android.presentation.calendar.navigation.Procedure
 import com.cherrish.android.presentation.calendar.procedure.model.ProcedureCardDisplayMode
 import com.cherrish.android.presentation.calendar.procedure.model.ProcedureCardItemUiModel
 import com.cherrish.android.presentation.calendar.procedure.model.ProcedureFlow
@@ -22,6 +24,7 @@ import com.cherrish.android.presentation.calendar.procedure.model.ProcedureWithD
 import com.cherrish.android.presentation.calendar.procedure.model.ProcedureWorryUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
+import java.time.LocalDate
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toImmutableList
 import kotlinx.collections.immutable.toPersistentList
@@ -31,7 +34,6 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import java.time.LocalDate
 
 @HiltViewModel
 class ProcedureViewModel @Inject constructor(
@@ -41,7 +43,10 @@ class ProcedureViewModel @Inject constructor(
     private val calendarEventBus: CalendarRefreshEventBus,
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
-    private val startDateArg = LocalDate.parse(savedStateHandle.get<String>("startDate"))
+
+    private val startDateArg = runCatching {
+        LocalDate.parse(savedStateHandle.toRoute<Procedure>().startDate)
+    }.getOrElse { LocalDate.now() }
 
     private val _uiState = MutableStateFlow<UiState<ProcedureUiState>>(
         UiState.Success(
@@ -51,6 +56,7 @@ class ProcedureViewModel @Inject constructor(
             )
         )
     )
+
     val uiState: StateFlow<UiState<ProcedureUiState>> = _uiState.asStateFlow()
 
     private val _completeEvent = MutableSharedFlow<Unit>(extraBufferCapacity = 1)
