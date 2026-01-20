f2bd48bf30545daf17d236092ac72c4499a7e8ed
Kim Nahyeon <nhyeonii@naver.com>
2026-01-20 22:23:50 +0900

    [CHORE/#113] Lint format

--- app/src/main/java/com/cherrish/android/presentation/onboarding/information/OnboardingInformationScreen.kt
+++ app/src/main/java/com/cherrish/android/presentation/onboarding/information/OnboardingInformationScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.cherrish.android.core.common.extension.addFocusCleaner
-import com.cherrish.android.core.common.extension.advancedImePadding
 import com.cherrish.android.core.common.extension.collectLatestSideEffect
 import com.cherrish.android.core.designsystem.component.button.CherrishButton
 import com.cherrish.android.core.designsystem.component.textfield.CherrishTextField
