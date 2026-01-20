f1d798434dc95318f419637cc52953a4ebe6d990
sohee <144779368+sohee6989@users.noreply.github.com>
2026-01-20 20:56:35 +0900

    [CHORE/#109] 파일명 수정

--- app/src/main/java/com/cherrish/android/data/remote/dto/response/ChallengeRoutineCompleteResponseDto.kt
+++ app/src/main/java/com/cherrish/android/data/remote/dto/response/ChallengeRoutineCompleteResponseDto.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ChallengeRoutineCompleteRequestDto(
+data class ChallengeRoutineCompleteResponseDto(
     @SerialName("routineId")
     val routineId: Long,
     @SerialName("name")
--- app/src/main/java/com/cherrish/android/data/remote/service/ChallengeMissionService.kt
+++ app/src/main/java/com/cherrish/android/data/remote/service/ChallengeMissionService.kt
@@ -2,9 +2,9 @@ package com.cherrish.android.data.remote.service
 
 import com.cherrish.android.core.network.BaseResponse
 import com.cherrish.android.data.remote.dto.response.ChallengeMissionResponseDto
+import com.cherrish.android.data.remote.dto.response.ChallengeRoutineCompleteResponseDto
 import retrofit2.http.GET
 import retrofit2.http.PATCH
-import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ChallengeMissionService {
@@ -14,5 +14,5 @@ interface ChallengeMissionService {
     @PATCH("api/demo/challenges/routines/{routineId}/toggle")
     fun patchChallengeRoutinesComplete(
         @Path("routineId") routineId: Long
-    )
+    ): BaseResponse<ChallengeRoutineCompleteResponseDto>
 }
