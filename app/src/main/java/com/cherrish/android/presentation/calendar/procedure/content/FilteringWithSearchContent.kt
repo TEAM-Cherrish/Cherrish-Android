90a351f9adb9b23da156b67cba27046dd4de5938
sssuhha <ysh83@sookmyung.ac.kr>
2026-01-20 22:20:16 +0900

    [FEAT/#90] FilteringWithSearchContent에 empty view 추가

--- app/src/main/java/com/cherrish/android/presentation/calendar/procedure/content/FilteringWithSearchContent.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/procedure/content/FilteringWithSearchContent.kt
@@ -1,5 +1,6 @@
 package com.cherrish.android.presentation.calendar.procedure.content
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -7,17 +8,23 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.cherrish.android.R
 import com.cherrish.android.core.designsystem.theme.CherrishTheme
 import com.cherrish.android.presentation.calendar.component.ProcedureTextField
 import com.cherrish.android.presentation.calendar.procedure.component.CautionDescription
@@ -121,21 +128,60 @@ fun FilteringWithSearchContent(
                 Spacer(modifier = Modifier.height(10.dp))
             }
 
-            items(
-                items = cardItems,
-                key = { it.id }
-            ) { item ->
-                ProcedureCard(
-                    procedureName = item.name,
-                    category = item.category,
-                    minDowntimeDays = item.minDowntimeDays,
-                    maxDowntimeDays = item.maxDowntimeDays,
-                    onCardClick = { onCardClick(item.id) },
-                    isSelected = item.id in selectedCardIds,
-                    displayMode = item.displayMode
-                )
+            if (cardItems.isEmpty()) {
+                item {
+                    EmptySearchResult(
+                        modifier = Modifier
+                            .fillParentMaxSize()
+                    )
+                }
+            } else {
+                items(
+                    items = cardItems,
+                    key = { it.id }
+                ) { item ->
+                    ProcedureCard(
+                        procedureName = item.name,
+                        category = item.category,
+                        minDowntimeDays = item.minDowntimeDays,
+                        maxDowntimeDays = item.maxDowntimeDays,
+                        onCardClick = { onCardClick(item.id) },
+                        isSelected = item.id in selectedCardIds,
+                        displayMode = item.displayMode
+                    )
+                }
             }
         }
+    }
+}
+
+@Composable
+private fun EmptySearchResult(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Spacer(modifier = Modifier.height(130.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.img_procedure_no_search),
+            contentDescription = null,
+            modifier = Modifier.size(98.dp, 106.dp)
+
+        )
+
+        Text(
+            text = "찾으시는 시술이 없습니다.\n" +
+                "다른 시술명을 입력해보세요.",
+            textAlign = TextAlign.Center,
+            style = CherrishTheme.typography.body1R14,
+            color = CherrishTheme.colors.gray600
+        )
+
+        Spacer(modifier = Modifier.height(126.dp))
     }
 }
 
@@ -161,6 +207,22 @@ private fun FilteringWithSearchContentPreview() {
             query = query,
             bottomPadding = 20.dp,
             onQueryChange = { query = it }
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+private fun FilteringWithSearchContentEmptyPreview() {
+    CherrishTheme {
+        FilteringWithSearchContent(
+            cardItems = persistentListOf(),
+            selectedCardIds = persistentListOf(),
+            onCardClick = {},
+            onSearchAction = {},
+            query = "레이저 토닝",
+            bottomPadding = 20.dp,
+            onQueryChange = {}
         )
     }
 }
