1dae807c620aeeb48e52c78741fe9949afedc732
sohee <144779368+sohee6989@users.noreply.github.com>
2026-01-20 21:11:57 +0900

    [FEAT/#109] model 생성

---
+++ app/src/main/java/com/cherrish/android/data/model/ChallengeMissionResponseModel.kt
@@ -0,0 +1,44 @@
+package com.cherrish.android.data.model
+
+import com.cherrish.android.data.remote.dto.response.ChallengeMissionResponseDto
+import com.cherrish.android.data.remote.dto.response.ChallengeRoutineResponseDto
+
+data class ChallengeMissionResponseModel(
+    val challengeId: Long,
+    val title: String,
+    val currentDay: Int,
+    val progressPercentage: Int,
+    val cherryLevel: Int,
+    val cherryLevelName: String,
+    val progressToNextLevel: Double,
+    val remainingRoutinesToNextLevel: Int,
+    val routines: List<ChallengeRoutineResponseModel>,
+    val cheeringMessage: String
+)
+
+data class ChallengeRoutineResponseModel(
+    val routineId: Long,
+    val name: String,
+    val scheduledDate: String,
+    val isComplete: Boolean
+)
+
+fun ChallengeMissionResponseDto.toModel() = ChallengeMissionResponseModel(
+    challengeId = this.challengeId,
+    title = this.title,
+    currentDay = this.currentDay,
+    progressPercentage = this.progressPercentage,
+    cherryLevel = this.cherryLevel,
+    cherryLevelName = this.cherryLevelName,
+    progressToNextLevel = this.progressToNextLevel,
+    remainingRoutinesToNextLevel = this.remainingRoutinesToNextLevel,
+    routines = this.routines.map { it.toModel() },
+    cheeringMessage = this.cheeringMessage
+)
+
+fun ChallengeRoutineResponseDto.toModel() = ChallengeRoutineResponseModel(
+    routineId = this.routineId,
+    name = this.name,
+    scheduledDate = this.scheduledDate,
+    isComplete = this.isComplete
+)
