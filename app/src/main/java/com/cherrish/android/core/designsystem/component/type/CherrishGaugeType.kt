073372a197c380648213595cfda945e7af9a2ab1
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-21 03:08:09 +0900

    [CHORE/103] CherrishGuageType 머지 병합
    # Conflicts:
    #	app/src/main/java/com/cherrish/android/core/designsystem/component/type/CherrishGaugeType.kt
    
    # Conflicts:
    #	app/src/main/java/com/cherrish/android/core/designsystem/component/type/CherrishGaugeType.kt

---
+++ app/src/main/java/com/cherrish/android/core/designsystem/component/type/CherrishGaugeType.kt
@@ -0,0 +1,15 @@
+package com.cherrish.android.core.designsystem.component.type
+
+import androidx.annotation.DrawableRes
+import com.cherrish.android.R
+
+enum class CherrishGaugeType(
+    val step: Int,
+    val percent: Int,
+    @DrawableRes val image: Int
+) {
+    LEVEL1(step = 1, percent = 25, image = R.drawable.img_challenge_lv1),
+    LEVEL2(step = 2, percent = 50, image = R.drawable.img_challenge_lv2),
+    LEVEL3(step = 3, percent = 75, image = R.drawable.img_challenge_lv3),
+    LEVEL4(step = 4, percent = 100, image = R.drawable.img_challenge_lv4)
+}
