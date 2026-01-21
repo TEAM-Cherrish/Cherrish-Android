d9169afeeaa78705108c8b548ed8bcbb125f1dde
Kim Nahyeon <nhyeonii@naver.com>
2026-01-22 03:46:44 +0900

    [FIX/#134] procedureInfoItem 다운타임 0일도 클릭 가능하게 수정

--- app/src/main/java/com/cherrish/android/presentation/calendar/component/ProcedureScheduleCard.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/component/ProcedureScheduleCard.kt
@@ -52,7 +52,6 @@ fun ProcedureScheduleCard(
     onAddProcedureClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val isDowntimeMode = displayMode is CalendarDisplayMode.Downtime
 
     Box(
         modifier = modifier
@@ -116,7 +115,6 @@ fun ProcedureScheduleCard(
                                     procedureId = procedure.procedureId,
                                     downTimeDuration = procedure.downTimeDuration
                                 ),
-                                isDowntimeMode = isDowntimeMode,
                                 onClick = { onProcedureClick(procedure.procedureId) }
                             )
                         }
