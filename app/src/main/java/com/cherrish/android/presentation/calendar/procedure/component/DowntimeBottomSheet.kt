0e559764f22eacd81ed4965ed6757c2fc5990565
sssuhha <ysh83@sookmyung.ac.kr>
2026-01-21 20:40:27 +0900

    [CHORE/#120] validationType 체크 우선순위 재정렬

--- app/src/main/java/com/cherrish/android/presentation/calendar/procedure/component/DowntimeBottomSheet.kt
+++ app/src/main/java/com/cherrish/android/presentation/calendar/procedure/component/DowntimeBottomSheet.kt
@@ -83,9 +83,9 @@ fun DowntimeBottomSheet(
         ) {
             val downtimeGuideBubbleText = when {
                 downtimeDay == 0 -> "회복 목표디데이로부터 약 ${spareTimeDay}일 전에 안정될 수 있어요."
-                validationType == DowntimeValidationType.INVALID -> ""
                 validationType == DowntimeValidationType.VALID ->
                     "회복 목표디데이로부터 약 ${spareTimeDay}일 전에 안정될 수 있어요."
+                validationType == DowntimeValidationType.INVALID -> ""
                 else -> "설정한 다운타임은 목표일을 넘깁니다."
             }
 
