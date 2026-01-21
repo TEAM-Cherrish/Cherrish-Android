837ad733052c99c8e6b7af8877200820b3d750d9
Kim Nahyeon <nhyeonii@naver.com>
2026-01-21 22:34:14 +0900

    [CHORE/#129] lint format

--- app/src/main/java/com/cherrish/android/presentation/challenge/missionprogress/ChallengeMissionProgressViewModel.kt
+++ app/src/main/java/com/cherrish/android/presentation/challenge/missionprogress/ChallengeMissionProgressViewModel.kt
@@ -121,7 +121,9 @@ class ChallengeMissionProgressViewModel @Inject constructor(
                     }
                     .onLogFailure { e ->
                         if (e is retrofit2.HttpException && e.code() == 404) {
-                            _sideEffect.emit(ChallengeMissionProgressSideEffect.NavigateToChallengeStart)
+                            _sideEffect.emit(
+                                ChallengeMissionProgressSideEffect.NavigateToChallengeStart
+                            )
                         }
                     }
             } finally {
