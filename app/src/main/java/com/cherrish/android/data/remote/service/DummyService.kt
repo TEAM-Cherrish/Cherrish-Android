e7ac90528f236c259328cb7fdb5cd65502feac8c
sohee <144779368+sohee6989@users.noreply.github.com>
2026-01-21 20:24:13 +0900

    [DEL/#117] 더미 파일 삭제

--- app/src/main/java/com/cherrish/android/data/remote/service/DummyService.kt
+++
@@ -1,8 +0,0 @@
-package com.cherrish.android.data.remote.service
-
-import com.cherrish.android.core.network.BaseResponse
-import com.cherrish.android.data.remote.dto.response.DummyResponseDto
-
-interface DummyService {
-    suspend fun getDummy(): BaseResponse<DummyResponseDto>
-}
--- app/src/main/java/com/cherrish/android/data/repository/DummyRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.cherrish.android.data.repository
-
-import com.cherrish.android.data.model.DummyModel
-
-interface DummyRepository {
-    suspend fun getDummy(): Result<DummyModel>
-}
