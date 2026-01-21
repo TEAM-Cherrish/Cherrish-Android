2cc8647728e32b5292298b41200d1902ec845490
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-21 17:30:39 +0900

    [FEAT/#121] Challenge Repository 구현

---
+++ app/src/main/java/com/cherrish/android/data/repository/ChallengeRepository.kt
@@ -0,0 +1,17 @@
+//ChallengeRepository.kt
+package com.cherrish.android.data.repository
+
+import com.cherrish.android.data.model.ChallengeCreateResponseModel
+import com.cherrish.android.data.model.ChallengeHomecareRoutinesResponseModel
+import com.cherrish.android.data.model.ChallengesAiRecommendResponseModel
+
+interface ChallengeRepository {
+    suspend fun getChallengeRoutineData(): Result<List<ChallengeHomecareRoutinesResponseModel>>
+    suspend fun postAiRecommendations(
+        homecareRoutineId: Int
+    ): Result<ChallengesAiRecommendResponseModel>
+    suspend fun postDemoChallenge(
+        homecareRoutineId: Int,
+        routineNames: List<String>
+    ): Result<ChallengeCreateResponseModel>
+}
