d6868dce200996964e87505ff3e79b388f425b6d
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-23 05:08:57 +0900

    [CHORE/#141] 충돌 해결

--- app/src/main/java/com/cherrish/android/presentation/challenge/loading/ChallengeLoadingScreen.kt
+++ app/src/main/java/com/cherrish/android/presentation/challenge/loading/ChallengeLoadingScreen.kt
@@ -1,6 +1,5 @@
 package com.cherrish.android.presentation.challenge.loading
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,7 +20,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
