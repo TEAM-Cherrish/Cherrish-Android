6e7074e36a869169441bf546d50aefc9f52bd5c0
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-01-22 21:30:57 +0900

    [FEAT/#141] Lottie 파일 적용 - ChallengeLoadingScreen - LoadingScreen

--- app/src/main/java/com/cherrish/android/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/cherrish/android/presentation/splash/SplashScreen.kt
@@ -1,26 +1,30 @@
 package com.cherrish.android.presentation.splash
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.cherrish.android.R
 import com.cherrish.android.core.common.extension.collectLatestSideEffect
 import com.cherrish.android.core.designsystem.theme.CherrishTheme
@@ -62,6 +66,15 @@ private fun SplashScreen(
 ) {
     val gradationColors = listOf(CherrishTheme.colors.gradation, CherrishTheme.colors.gradation2)
 
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.lt_challenge_loading)
+    )
+    val progress by animateLottieCompositionAsState(
+        composition,
+        iterations = LottieConstants.IterateForever,
+        isPlaying = true
+    )
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -77,18 +90,17 @@ private fun SplashScreen(
     ) {
         Spacer(modifier = Modifier.weight(283f))
 
-        Image(
-            painter = painterResource(id = R.drawable.ic_app_logo),
-            contentDescription = null,
-            modifier = Modifier.size(width = 114.dp, height = 100.dp)
+        LottieAnimation(
+            composition = composition,
+            progress = { progress },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 105.dp)
+                .aspectRatio(130f / 154f)
+                .align(Alignment.CenterHorizontally)
         )
 
         Spacer(modifier = Modifier.height(14.dp))
-
-        Image(
-            imageVector = ImageVector.vectorResource(id = R.drawable.ic_app_logo_title),
-            contentDescription = null
-        )
 
         Spacer(modifier = Modifier.weight(298f))
     }
